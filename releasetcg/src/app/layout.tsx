fadbc49e07c00c2c9271f380d27b4b28a4d58a00
infiniteuno22 <hungvnguyen922@gmail.com>
2026-05-23 00:22:30 -0500

    SCRUM-9 Side bar component downloaded

--- releasetcg/src/app/layout.tsx
+++ releasetcg/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Figtree } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const figtree = Figtree({
   subsets: ["latin"],
