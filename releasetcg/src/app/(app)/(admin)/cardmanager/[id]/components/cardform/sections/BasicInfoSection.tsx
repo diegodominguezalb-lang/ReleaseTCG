f8ad5d719a851322faffa717dfa3858cbd21ff21
infiniteuno22 <hungvnguyen922@gmail.com>
2026-06-19 19:03:05 -0500

    SCRUM-23 divides card modification into separate sections

---
+++ releasetcg/src/app/(app)/(admin)/cardmanager/[id]/components/cardform/sections/BasicInfoSection.tsx
@@ -0,0 +1,52 @@
+import { CardForm, UpdateCard } from "../types";
+import { CardSection } from "../CardSection";
+import { TextInput } from "../fields/TextInput";
+import { NumberInput } from "../fields/NumberInput";
+
+type Props = {
+  form: CardForm;
+  update: UpdateCard;
+};
+
+export function BasicInfoSection({
+  form,
+  update,
+}: Props) {
+  return (
+    <CardSection title="Basic Information">
+      <TextInput
+        label="Name"
+        value={form.name}
+        onChange={(value) =>
+          update("name", value)
+        }
+      />
+
+      <div className="grid grid-cols-2 gap-4">
+        <NumberInput
+          label="Power"
+          value={form.power}
+          onChange={(value) =>
+            update("power", value)
+          }
+        />
+
+        <NumberInput
+          label="Bulk"
+          value={form.bulk}
+          onChange={(value) =>
+            update("bulk", value)
+          }
+        />
+      </div>
+
+      <TextInput
+        label="Trait"
+        value={form.trait}
+        onChange={(value) =>
+          update("trait", value)
+        }
+      />
+    </CardSection>
+  );
+}
