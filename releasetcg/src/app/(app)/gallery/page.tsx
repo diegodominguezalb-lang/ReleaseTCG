d219d728c08164c9e946003d28ed43956c24c361
infiniteuno22 <hungvnguyen922@gmail.com>
2026-06-21 12:53:19 -0500

    SCRUM-25 Displays cards on gallery page

--- releasetcg/src/app/(app)/gallery/page.tsx
+++ releasetcg/src/app/(app)/gallery/page.tsx
@@ -1,9 +1,12 @@
-export default async function GalleryPage() {
+import { Gallery } from "./Gallery";
+import { getPublicCards } from "@/utils/supabase/cards/getPublicCards";
 
+export default async function GalleryPage() {
+  const cards = await getPublicCards();
 
   return (
     <main className="h-screen overflow-hidden">
-
+      <Gallery initialCards={cards} />
     </main>
   );
 }
