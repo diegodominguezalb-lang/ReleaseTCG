7a18f1136b9112f0d2edfe9e4fb09755c79ba022
infiniteuno22 <hungvnguyen922@gmail.com>
2026-06-22 12:03:19 -0500

    SCRUM-27 card modal laid out

---
+++ releasetcg/src/features/cards/CardModal.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import { CardDetails } from "@/types/cards";
+
+import { CardViewer } from "./viewer/CardViewer";
+
+type Props = {
+  card: CardDetails | null;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+};
+
+export function CardModal({
+  card,
+  open,
+  onOpenChange,
+}: Props) {
+  if (!card) {
+    return null;
+  }
+
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={onOpenChange}
+    >
+      <DialogContent
+        className="
+            w-[95vw]
+            max-w-[1400px]
+            h-[90vh]
+            sm:max-w-[50vw]
+            rounded-3xl
+            overflow-hidden
+            p-0
+            border-0
+            shadow-none
+            bg-transparent
+        "
+    >
+        <DialogHeader className="sr-only">
+            <DialogTitle>{card.name}</DialogTitle>
+        </DialogHeader>
+
+        <CardViewer card={card} />
+      </DialogContent>
+    </Dialog>
+  );
+}
